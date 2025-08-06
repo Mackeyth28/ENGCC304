ed5d64e40e5ae23d30613d13aa93d267684b2976
Mackeyth28 <thapkrncingam@gmail.com>
2025-08-06 23:11:26 +0700

    Lab

---
+++ Homework/Hw1/Hw1-3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char F[50] ;
+    char L[50] ;
+    printf("First Name") ;
+    scanf("%s", F) ;
+
+    printf("Last Name") ;
+    scanf("%s", L) ;
+    
+    printf("%s %s TC, RMUTL, Chiang Mai, Thailand", F,L) ;
+    return 0 ;
+}
