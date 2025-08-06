1f833eb60d529d4d90f4c4be569c7f127d2c8f07
Mackeyth28 <thapkrncingam@gmail.com>
2025-08-06 22:33:53 +0700

    Lab

---
+++ Hw/Hw1/Hw.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h> //นำเข้าคำสั่งทั้งหมดของไฟล์ stdio.h (header file)
+
+int main () //main คือฟังก์ชั้นสำหรับเริ่มต้นโปรเเกรม
+{
+    /*
+    เเสดงผลข้อมูลโดยใช้ฟังก์ชั่นprintf
+    ที่อยู่ในไฟล์ของ stdio.h
+    */
+    printf("Hello world") ; //เเสดงข้อความ Hello world
+}    
